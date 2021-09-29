3cbbba154cf4941dd2d54fb2a240c6b1b9bd1030
Yordan Dobrev <42092212+YordanDobrev97@users.noreply.github.com>
2021-09-29 19:26:38 +0300

    Remove redundant files

--- website/auth.py
+++ website/auth.py
@@ -1,14 +1,18 @@
 from website import views
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request
 
 auth = Blueprint('auth', __name__)
 
 
-@auth.route('/login')
+@auth.route('/login', methods=['GET'])
 def login():
     return render_template('login.html')
 
 
-@auth.route('/register')
+@auth.route('/register', methods=['GET', 'POST'])
 def register():
-    return render_template('register.html')
+    if request.method == 'GET':
+        return render_template('register.html')
+    else:
+        pass
+
