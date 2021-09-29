c91764d2baa548de74d9dc4d4e0baab12f459454
Yordan Dobrev <42092212+YordanDobrev97@users.noreply.github.com>
2021-09-29 19:13:29 +0300

    Init Flask App

---
+++ website/auth.py
@@ -0,0 +1,14 @@
+from website import views
+from flask import Blueprint, render_template
+
+auth = Blueprint('auth', __name__)
+
+
+@auth.route('/login')
+def login():
+    return render_template('login.html')
+
+
+@auth.route('/register')
+def register():
+    return render_template('register.html')
